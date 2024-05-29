98d44570f22e05968c9025741b5fa1640e40c73b
Quan2808 <116139754+Quan2808@users.noreply.github.com>
2024-05-29 13:23:34 +0700

    create api

---
+++ apollo-bookstore/src/test/java/com/apollo/bookstore/apollo_bookstore/ApolloBookstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apollo.bookstore.apollo_bookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApolloBookstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
